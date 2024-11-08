69ab54c641c57ebb665f43ea7afe921e30beff09
CanseFr <jucansell@gmail.com>
2024-11-08 12:01:59 +0100

    :sparkles: Add some stuff to test with cypress

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,29 +1,39 @@
-import React from "react";
+import React, { useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // useEffect(()=>{
-  //   fetch('http://localhost:3001').then(res => res.json()).then(json => console.log(json));
-  // },[])
+  const [data, setData] = React.useState<any[]>([]);
+  const [numb, setNumb] = React.useState<number>(0);
+
+  useEffect(() => {
+    fetch('http://localhost:3001/articles')
+      .then((res) => res.json())
+      .then(setData);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Cypress Ci Test</p>
+        <h4>Data from Db</h4>
+        <div>
+          {data.map((d) => (
+            <div key={d.id} style={{ backgroundColor: "white", borderRadius: "5px", color: "black", padding: "5px" }}>
+              <h3>{d.title}</h3>
+              <p>{d.description}</p>
+            </div>
+          ))}
+        </div>
+
+        <h4>Click testing</h4>
+        <p>Count : {numb}</p>
+        <button onClick={() => setNumb(numb + 1)}>Increment</button>
       </header>
     </div>
   );
 }
+
 
 export default App;
